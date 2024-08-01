93338d92c16276a764f45c7314622e158f7e9421
burstCode <yourewelcomex@gmail.com>
2024-08-01 21:03:37 +0300

    Предварительная проверка запроса посредством токена #1

--- EGISZtemplatesAPI/Program.cs
+++ EGISZtemplatesAPI/Program.cs
@@ -1,5 +1,6 @@
 using EGISZtemplatesAPI.Models;
 using EGISZtemplatesAPI.Data;
+using EGISZtemplatesAPI.Middleware;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,6 +27,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<ApiKeyMiddleware>();
 
 app.MapControllers();
 
